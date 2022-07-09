ad4b8886fb041d49a2425a1bb593f7c71fe01176
nikolay <admin@kgd.in>
2022-07-09 14:23:51 +0200

    Initial

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Calculator;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase {
+
+  /**
+   * @covers Calculator::add
+   */
+  public function testAddOperation(): void {
+    $adder = new Calculator("add");
+    $value = $adder->evaluate(2, 3);
+    $this->assertEquals(5, $value);
+  }
+
+  /**
+   * @covers Calculator::subtract
+   */
+  public function testSubtractOperation(): void {
+    $subtract = new Calculator("subtract");
+    $value = $subtract->evaluate(2, 3);
+    $this->assertEquals(-1, $value);
+  }
+}
